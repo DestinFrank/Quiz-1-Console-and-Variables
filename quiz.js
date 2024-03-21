cd7e465c073cb96ec0c11dea0dd6eb78c2f8c9a2
Destin Frank <159183449+DestinFrank@users.noreply.github.com>
2024-03-21 13:27:52 -0400

    Updated Quiz Part

--- quiz.js
+++ quiz.js
@@ -1,9 +1,25 @@
 const prompt = require('prompt-sync')();
 
 //Program 1: Temperature Converter
-
+let deg = prompt('What is the temperature in Farenheit?');
+console.log('The temperature is ' + (deg - 32)*5/9 + ' in celsius');
 //Program 2: Grade Calculator
-
+let grade = prompt('What percentage did you get on the test?');
+if (grade > 89){
+    console.log('Wow! You got an A!');
+}
+else if (grade > 79){
+    console.log('Good job, you got a B.');
+}
+else if (grade > 69){
+    console.log('At least you passed, you got a C.');
+}
+else if (grade > 59){
+    console.log('Careful, you almost failed! You got a D.');
+}
+else{
+    console.log('Bruh, you failed.');
+}
 //Program 3: Leap Year Checker
 
 //Program 4: Largest Number Finder
